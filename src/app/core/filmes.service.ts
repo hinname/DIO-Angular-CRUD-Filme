20c2ba11fc3e607dfa3ca59e6ddbdfb277b4bc59
Herivelton Costa <herivelton.PRo@gmail.com>
2020-09-16 17:46:55 -0300

    filter e infinite scroll

---
+++ src/app/core/filmes.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ConfigParams } from '../shared/models/config-params';
+import { Filme } from '../shared/models/filme';
+import { ConfigParamsService } from './config-params.service';
+
+const url = "http://localhost:3000/filmes";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FilmesService {
+
+
+  constructor(private http: HttpClient,
+              private configService: ConfigParamsService) { }
+
+  salvar(filme: Filme): Observable<Filme> {
+    return this.http.post<Filme>(url, filme);
+  }
+
+  listar(config: ConfigParams): Observable<Filme[]> {
+    const configParams = this.configService.configurarParametros(config);
+
+    return this.http.get<Filme[]>(url, { params: configParams});
+  }
+}
